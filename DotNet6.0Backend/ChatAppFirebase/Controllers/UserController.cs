ad8a7c74ba1be685890f08d7ecec5684c54edc79
Jiwan <114226099+gurjeetsingh622001@users.noreply.github.com>
2024-06-17 12:39:41 +0530

    create user

---
+++ DotNet6.0Backend/ChatAppFirebase/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using FirebaseAdmin.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Services.fbInterface;
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace ChatAppFirebase.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private IfbUserServices _fbUserServices;
+        public UserController(IfbUserServices _fbUserService) { 
+            _fbUserServices = _fbUserService;
+        }
+        [HttpPost]
+        public Task<UserRecord> CreateUser(string user)
+        {
+            return _fbUserServices.CreateUser(user);
+        }
+    }
+}
